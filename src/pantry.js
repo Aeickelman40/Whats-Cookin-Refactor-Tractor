118eaa595df4fd714448ff862cc50d2805c0e043
Andy Tom <andrew.tom@colorado.edu>
2020-05-28 20:28:39 -0600

    Remakes checkRecipeStatus method refactoring checkPantry

--- src/pantry.js
+++ src/pantry.js
@@ -1,23 +1,15 @@
+import ingredientsData from "./data/ingredients";
 
-import data from "./scripts";
 class Pantry {
   constructor(user, contents) {
     this.user = user;
     this.contents = contents;
-    this.counter = 0;
-    this.missingIngredients = [];
+    this.missingIngredients = []
   }
-  checkPantry(recipeIngredients) {
-  // Break down into smaller functions, this could maybe be a helper function
-
-    console.log('this.missingIngredients', this.missingIngredients);
-    if (this.checkRecipeStatus()) {
-      this.user.mealList.push(recipe)
-    }
-    this.findIngredient(recipeIngredients)
+  checkPantry() {
     return this.missingIngredients.map(ingredient => {
       let tempEstimatedCost;
-      data.ingredientsData.find(specificIngredient => {
+      ingredientsData.find(specificIngredient => {
         if (specificIngredient.id === ingredient.id) {
           tempEstimatedCost = specificIngredient.estimatedCostInCents
         }
@@ -25,35 +17,23 @@ class Pantry {
       return {
         id: ingredient.id,
         name: ingredient.name,
-        estimatedCostInCents: tempEstimatedCost,
-        unit: ingredient.quantity.unit
+        estimatedCostInCents: tempEstimatedCost
       }
     })
-
   }
-  findIngredient(recipeIngredients) {
+  checkRecipeStatus(recipeIngredients) {
     let counter = 0;
     recipeIngredients.forEach(recipeIngredient => {
       this.user.pantry.contents.forEach(pantryItem => {
         if (pantryItem.ingredient === recipeIngredient.id) {
-          this.counter++;
+          counter++;
         } else if (!this.missingIngredients.includes(recipeIngredient)) {
           this.missingIngredients.push(recipeIngredient);
         }
-  
       })
     })
- 
-    return this.missingIngredients;
-  }
-
-  checkRecipeStatus(recipeIngredients) {
-    if (this.counter === recipeIngredients.length + 1) {
-      
-      //window.alert('You have the ingredients!');
-      return true;
-    } else {
-      //window.alert(`You don't have enough ingredients, check your shopping list!`);
+    if (counter === recipeIngredients.length) {
+      return 'You have the ingredients!';
     }
   }
 }
--- src/user.js
+++ src/user.js
@@ -41,16 +41,16 @@ class User {
     if (typeof recipe !== 'object') {
       this.mealList.push(null);
       return;
-    } else if (!this.mealList.includes(recipe) && this.pantry.checkRecipeStatus(recipe.ingredients)) {
+    } else if (!this.mealList.includes(recipe) && this.pantry.checkRecipeStatus(recipe.ingredients) === 'You have the ingredients!') {
       this.mealList.push(recipe);
       console.log(this.mealList);
     } else if (!this.mealList.includes(recipe)) {
-      let tempIngredients = this.pantry.checkPantry(recipe.ingredients);
+      let tempIngredients = this.pantry.checkPantry();
       console.log( 'tempIngredients', tempIngredients)
       let newIngredients = this.shoppingList.concat(tempIngredients);
       let uniqIngredients = [...new Set(newIngredients)];
       this.shoppingList = uniqIngredients;
-      // console.log(this.shoppingList)
+      console.log(this.shoppingList)
     }
   }
 }
