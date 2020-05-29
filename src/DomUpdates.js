10e860a16cc45ccfb18db6a47ea9bacef6965c7d
Andy Tom <andrew.tom@colorado.edu>
2020-05-29 11:26:57 -0600

    Adds new methods for domUpdates, populateCardsHTML and returnDirectionsInnerHTML

--- src/DomUpdates.js
+++ src/DomUpdates.js
@@ -1,4 +1,28 @@
 class DomUpdates {
-    
+  returnDirectionsInnerHTML(cardArea,recipeObject, costInDollars) {
+    cardArea.innerHTML = `<h3>${recipeObject.name}</h3>
+    <p class='all-recipe-info'>
+    <strong>It will cost: </strong><span class='cost recipe-info'>
+    $${costInDollars}</span><br><br>
+    <strong>You will need: </strong><span class='ingredients recipe-info'></span>
+    <strong>Instructions: </strong><ol><span class='instructions recipe-info'>
+    </span></ol>
+    <strong> Tags: </strong><ol><span class='recipe-tags recipe-info'></span></ol>
+    <p>`
+  }
+  populateCardsHTML(cardArea, recipe) {
+    cardArea.insertAdjacentHTML('afterbegin', 
+      `<section id='${recipe.id}'class='card'>
+        <header id='${recipe.id}' class='card-header'>
+          <label for='add-button' class='hidden'>Click to add recipe</label>
+          <button id='${recipe.id}' aria-label='add-button' class='add-button card-button'>
+          add</button>
+          <button id='${recipe.id}' aria-label='favorite-button' class='favorite favorite${recipe.id} card-button'></button>
+        </header>
+          <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
+          <img id='${recipe.id}' tabindex='0' class='card-picture'
+          src='${recipe.image}' alt='click to view recipe for ${recipe.name}'>
+    </section>`)
+  }
 }
 export default DomUpdates;
