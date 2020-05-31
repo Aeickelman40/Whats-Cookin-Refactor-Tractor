64470a833b56e7ff07304ea565ed17549c5cf304
Andy Tom <andrew.tom@colorado.edu>
2020-05-31 14:15:50 -0600

    Tried to put spies on document.insertAdjacentHTML

--- test/dom-updates-test.js
+++ test/dom-updates-test.js
@@ -1,16 +1,18 @@
 const chai = require('chai');
 import { expect } from 'chai';
 import DomUpdates from '../src/DomUpdates';
+import recipeTestData from '../src/test-data/recipes-test-data'
 const spies = require('chai-spies');
 chai.use(spies);
-
 let domUpdates;
+let cardArea;
 
 describe('DomUpdates', () => {
   beforeEach(() => {
     domUpdates = new DomUpdates();
     global.document = {};
-    chai.spy.on(document, ['innerHTML'], () => { })
+    chai.spy.on(document, ['insertAdjacentHTML'], () => {});
+    //cardArea = document.querySelector('.all-cards')
   });
 
   it('Should be a function', () => {
@@ -21,8 +23,9 @@ describe('DomUpdates', () => {
     expect(domUpdates).to.be.an.instanceOf(DomUpdates);
   });
 
-  it.skip('Should have an array of all recipes', () => {
-    expect(cookbook.recipes).to.be.an('array');
+  it.skip('Should call insertAdjacentHTML correctly', () => {
+    domUpdates.populateCardsHTML(cardArea, recipeTestData[0]);
+    expect(document.insertAdjacentHTML).to.have.been.called(1);
   });
 
   it.skip('Should be able to filter through its array by ingredients', () => {
